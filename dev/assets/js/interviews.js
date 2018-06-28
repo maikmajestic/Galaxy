dbc11f7dbb5d8bf60568230345b8b11ab16c62c6
mlopezs01 <mlopezs01@liverpool.com.mx>
2018-06-28 15:09:47 -0500

    Version 2

--- dev/assets/js/interviews.js
+++ dev/assets/js/interviews.js
@@ -41,6 +41,38 @@ $( document ).ready(function() {
 	   		$('#questions').append('<div class="col-8 item-member"><div class="row align-items-center"><div class="col-9 item-link"><a class="text-white text-center" href="video.html?a=fz&v='+i+'">'+questions[i]+'</a></div><div class="col-3 item-link-request"><a href="video.html?a=fz&v='+i+'"><i class="fas fa-play"></i></a></div></div></div>');
 	   	}
 		}
+		if (window.location.href.indexOf("?a=jg") > -1) {
+	  	$('#id-author').text('Javier Garciadiego');
+	  	var questions = ['Importancia de la historia', 'Importancia de conocer la revolución mexicana', 'Aportaciones de la revolución al país actual', 'Importancia de libertad de expresión y derechos a la información en la construcción de un país', 'Papel de los jóvenes en las transformaciones del país', 'Importancia de una institución como El Colegio Nacional'];
+	  	var noquestions = questions.length;
+	  	for(var i=0;i<noquestions;i++){
+	   		$('#questions').append('<div class="col-8 item-member"><div class="row align-items-center"><div class="col-9 item-link"><a class="text-white text-center" href="video.html?a=jg&v='+i+'">'+questions[i]+'</a></div><div class="col-3 item-link-request"><a href="video.html?a=jg&v='+i+'"><i class="fas fa-play"></i></a></div></div></div>');
+	   	}
+		}
+		if (window.location.href.indexOf("?a=jc") > -1) {
+	  	$('#id-author').text('José Ramón Cossío');
+	  	var questions = ['El derecho constitucional', 'Los derechos sociales', 'Clasificación de los derechos', 'Relación entre el derecho y otras disciplinas', 'Promover cambios en la estructura y redacción de las sentencias', 'Maneras de contribuir o generar un mejor Estado', 'Deber de un juzgador mexicano', 'El valor de su labor como docente, investigador y servidor público', 'Importancia de una institución como El Colegio Nacional'];
+	  	var noquestions = questions.length;
+	  	for(var i=0;i<noquestions;i++){
+	   		$('#questions').append('<div class="col-8 item-member"><div class="row align-items-center"><div class="col-9 item-link"><a class="text-white text-center" href="video.html?a=jc&v='+i+'">'+questions[i]+'</a></div><div class="col-3 item-link-request"><a href="video.html?a=jc&v='+i+'"><i class="fas fa-play"></i></a></div></div></div>');
+	   	}
+		}
+		if (window.location.href.indexOf("?a=lv") > -1) {
+	  	$('#id-author').text('Luis Villoro');
+	  	var questions = ['Sobre la filosofía y por qué dedicarse a ella', 'Sobre la equidad', 'Sobre la posrevolución', 'Pendientes del proceso histórico mexicano', 'Relación entre la cultura local y la cultura universal'];
+	  	var noquestions = questions.length;
+	  	for(var i=0;i<noquestions;i++){
+	   		$('#questions').append('<div class="col-8 item-member"><div class="row align-items-center"><div class="col-9 item-link"><a class="text-white text-center" href="video.html?a=lv&v='+i+'">'+questions[i]+'</a></div><div class="col-3 item-link-request"><a href="video.html?a=lv&v='+i+'"><i class="fas fa-play"></i></a></div></div></div>');
+	   	}
+		}
+		if (window.location.href.indexOf("?a=mp") > -1) {
+	  	$('#id-author').text('Miguel León Portilla');
+	  	var questions = ['Importancia de las lenguas indígenas', 'Preservación de las lenguas indígenas', 'Diferencia entre una lengua y un dialecto', 'Importancia de conocer la versión de un pueblo vencido', 'Herencia de la vida prehispánica', 'Importancia de una institución como El Colegio Nacional'];
+	  	var noquestions = questions.length;
+	  	for(var i=0;i<noquestions;i++){
+	   		$('#questions').append('<div class="col-8 item-member"><div class="row align-items-center"><div class="col-9 item-link"><a class="text-white text-center" href="video.html?a=mp&v='+i+'">'+questions[i]+'</a></div><div class="col-3 item-link-request"><a href="video.html?a=mp&v='+i+'"><i class="fas fa-play"></i></a></div></div></div>');
+	   	}
+		}
 	}
 
 	if (window.location.href.indexOf("video.html") > -1) {
@@ -62,6 +94,38 @@ $( document ).ready(function() {
 		}
 		if (window.location.href.indexOf("?a=fz") > -1) {
 		  var videos = ['../../assets/videos/interviews/fixZamudio/1.mov', '../../assets/videos/interviews/fixZamudio/2.mov', '../../assets/videos/interviews/fixZamudio/3.mov', '../../assets/videos/interviews/fixZamudio/4.mov', '../../assets/videos/interviews/fixZamudio/5.mov', '../../assets/videos/interviews/fixZamudio/6.mov'];
+		  var vid = getUrlParameter('v');
+		  console.log(vid);
+		  $('#video source').attr('src', videos[vid]);
+		  $("#video video")[0].load();
+		  $('video').trigger('play');
+		}
+		if (window.location.href.indexOf("?a=jg") > -1) {
+		  var videos = ['../../assets/videos/interviews/javierGarciadego/historia.mov', '../../assets/videos/interviews/javierGarciadego/importanterevolucion.mov', '../../assets/videos/interviews/javierGarciadego/revolucion.mov', '../../assets/videos/interviews/javierGarciadego/expresion.mov', '../../assets/videos/interviews/javierGarciadego/jovenes.mov', '../../assets/videos/interviews/javierGarciadego/importancia.mov'];
+		  var vid = getUrlParameter('v');
+		  console.log(vid);
+		  $('#video source').attr('src', videos[vid]);
+		  $("#video video")[0].load();
+		  $('video').trigger('play');
+		}
+		if (window.location.href.indexOf("?a=jc") > -1) {
+		  var videos = ['../../assets/videos/interviews/joseCossio/constitucional.mov', '../../assets/videos/interviews/joseCossio/sociales.mov', '../../assets/videos/interviews/joseCossio/derechos.mov', '../../assets/videos/interviews/joseCossio/disciplinas.mov', '../../assets/videos/interviews/joseCossio/promover.mov', '../../assets/videos/interviews/joseCossio/sociedad.mov', '../../assets/videos/interviews/joseCossio/juzgador.mov', '../../assets/videos/interviews/joseCossio/docente.mov', '../../assets/videos/interviews/joseCossio/importancia.mov'];
+		  var vid = getUrlParameter('v');
+		  console.log(vid);
+		  $('#video source').attr('src', videos[vid]);
+		  $("#video video")[0].load();
+		  $('video').trigger('play');
+		}
+		if (window.location.href.indexOf("?a=lv") > -1) {
+		  var videos = ['../../assets/videos/interviews/luisVilloro/filosofia.mov', '../../assets/videos/interviews/luisVilloro/equidad.mov', '../../assets/videos/interviews/luisVilloro/posrevolucion.mov', '../../assets/videos/interviews/luisVilloro/pendientes.mov', '../../assets/videos/interviews/luisVilloro/cultura.mov'];
+		  var vid = getUrlParameter('v');
+		  console.log(vid);
+		  $('#video source').attr('src', videos[vid]);
+		  $("#video video")[0].load();
+		  $('video').trigger('play');
+		}
+		if (window.location.href.indexOf("?a=mp") > -1) {
+		  var videos = ['../../assets/videos/interviews/miguelPortilla/lenguas.mov', '../../assets/videos/interviews/miguelPortilla/indigenas.mov', '../../assets/videos/interviews/miguelPortilla/dialecto.mov', '../../assets/videos/interviews/miguelPortilla/pueblo.mov', '../../assets/videos/interviews/miguelPortilla/importancia.mov'];
 		  var vid = getUrlParameter('v');
 		  console.log(vid);
 		  $('#video source').attr('src', videos[vid]);
