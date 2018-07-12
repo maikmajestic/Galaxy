ce5a885f8fcbe6e3cde7b4ff6508798802fba840
Fernando Salgado <nine.crumor@gmail.com>
2018-07-12 07:40:57 -0500

    changes

--- dev/assets/js/universe.js
+++ dev/assets/js/universe.js
@@ -1,4 +1,3 @@
-
 $(function() {
   $(".object").draggable({
     containment: "body",
@@ -15,6 +14,20 @@ $(function() {
       description.removeClass("hidden");
     } else {
       description.addClass("hidden");
+    }
+  });
+  $(document).keydown(function(event) {
+    if (event.ctrlKey == true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109' || event.which == '187' || event.which == '189')) {
+      event.preventDefault();
+    }
+    // 107 Num Key  +
+    // 109 Num Key  -
+    // 173 Min Key  hyphen/underscor Hey
+    // 61 Plus key  +/= key
+  });
+  $(window).bind('mousewheel DOMMouseScroll', function(event) {
+    if (event.ctrlKey == true) {
+      event.preventDefault();
     }
   });
 });
